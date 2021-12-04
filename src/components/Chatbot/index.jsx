4cb795afbcc55b2b0db4bf3fefc3944c120cd7c2
Ayush Singhal <ayushsinghal1502@gmail.com>
2021-12-04 14:35:32 +0530

    initial commit

---
+++ src/components/Chatbot/index.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+
+import BotMessage from "../BotMessage";
+import UserMessage from "../UserMessage";
+import Messages from "../Messages";
+import Input from "../Input";
+
+import Header from "../Header";
+import API from "../../api/chatBotApi";
+
+const Chatbot = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    async function loadWelcomeMessage() {
+      setMessages([
+        <BotMessage
+          key="0"
+          fetchMessage={ async () => await API.GetChatbotResponse("hi") }
+        />
+      ]);
+    }
+    loadWelcomeMessage();
+  }, []);
+
+  const send = async text => {
+    const newMessages = messages.concat(
+      <UserMessage key={ messages.length + 1 } text={ text } />,
+      <BotMessage
+        key={ messages.length + 2 }
+        fetchMessage={ async () => await API.GetChatbotResponse(text) }
+      />
+    );
+    setMessages(newMessages);
+  };
+
+  return (
+    <div className="chatbot">
+      <Header />
+      <Messages messages={ messages } />
+      <Input onSend={ send } />
+    </div>
+  );
+}
+
+export default Chatbot;
